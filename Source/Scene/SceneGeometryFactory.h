9f4e7f49d8c79376dd20588eb3d5484c7471f31d
fifty <fifty@nimeshdesai.name>
2026-06-02 02:55:08 +1000

    Adding Sphere geometry and solving Ray equation using Geometric solution.
    Also tested generation of sphere and square and when square placed behind, scene appears as expected. i.e. depth testing is working well.

--- Source/Scene/SceneGeometryFactory.h
+++ Source/Scene/SceneGeometryFactory.h
@@ -15,8 +15,8 @@ public:
 		return new Square(_p, _n, _c, _hs);
 	}
 
-	static Sphere* GenerateSphere(Point _p, Vector _n, RGBColor _c, float _hs) {
-		// todo
-		return new Sphere();
+	static Sphere* GenerateSphere(Point _p, RGBColor _c, float _r) {
+		Sphere* s = new Sphere(_p, _c, _r);
+		return s;
 	}
 };
